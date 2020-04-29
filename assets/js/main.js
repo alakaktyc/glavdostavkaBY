cd1fa408c7c04f2b907af7f6a6c1962318009a1e
alakaktyc <alakaktyc@gmail.com>
2020-04-29 15:39:40 +0300

    feat: add news slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,11 @@
+//мобильное меню
 const btnMenu = document.querySelector('.menu-btn');
 btnMenu.addEventListener('click', function () {
     const mobileMenu = document.querySelector('.navigation-box');
     mobileMenu.classList.toggle('active')
 });
 
+//переключатель объём/габариты
 const switchParameter = document.querySelector('.form__switch');
 switchParameter.addEventListener('click', function (event) {
     let parameter = document.querySelectorAll('.form__switch span');
@@ -25,11 +27,11 @@ switchParameter.addEventListener('click', function (event) {
     }
 });
 
-
+//Виджеты
 const widgets = document.querySelector('.widgets-btn');
 window.addEventListener('scroll', function () {
     let st = document.documentElement.scrollTop;
-    if (window.innerWidth >= 720) {
+    if (window.innerWidth >= 1366) {
         if (st > 300) {
             widgets.classList.add('active')
         } else {
@@ -41,33 +43,121 @@ window.addEventListener('scroll', function () {
 });
 
 
-$(document).ready(function(){
-    $('.news__slider').slick({
-        dots: false,
-        infinite: false,
-        speed: 300,
-        initialSlide: 3,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        variableWidth: true,
-        adaptiveHeight: true,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
+//main slider
+let slideIndex = 1;
+let slideInterval = setInterval(showSlides, 3500);
+const slides = document.querySelectorAll('.slide');
+const blockControlSlides = document.querySelector('.main-slider__control');
+
+function getListControls() {
+    let listControls = [];
+    for (let i = 1; i <= slides.length; i++) {
+        let li = document.createElement('li');
+        li.className = 'circle';
+        listControls.push(li);
+    }
+    return listControls;
+}
+
+blockControlSlides.append(...getListControls());
+
+const btnControlsSlider = document.querySelectorAll('.main-slider__control li');
+for (let i = 0; i < btnControlsSlider.length; ++i) {
+    btnControlsSlider[i].addEventListener('click', function () {
+        clearInterval(slideInterval);
+        currentSlide(slideIndex = i + 1);
+        slideInterval = setInterval(showSlides, 3500);
+    });
+}
+
+function showSlides(n) {
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].className = 'slide';
+    }
+    for (let i = 0; i < btnControlsSlider.length; i++) {
+        btnControlsSlider[i].className = btnControlsSlider[i].className = 'circle';
+    }
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    slides[slideIndex - 1].className = 'slide active';
+    btnControlsSlider[slideIndex - 1].className = 'circle show';
+    slideIndex++
+}
+
+for (let i = 0; i < slides.length; ++i) {
+    slides[i].addEventListener('mouseenter', function () {
+        clearInterval(slideInterval);
+    });
+    slides[i].addEventListener('mouseleave', function () {
+        slideInterval = setInterval(showSlides, 3500);
+    });
+}
+
+//swiper
+const getUseZone = document.querySelector('.main-slider');
+let touchstartX = 0;
+let touchendX = 0;
+getUseZone.addEventListener('touchstart', function (event) {
+    touchstartX = event.changedTouches[0].screenX;
+}, false);
+
+getUseZone.addEventListener('touchend', function (event) {
+    touchendX = event.changedTouches[0].screenX;
+    handleMove();
+}, false);
+
+function handleMove() {
+    if (touchendX <= touchstartX) {
+        clearInterval(slideInterval);
+        showSlides(slideIndex + 1);
+        slideInterval = setInterval(showSlides, 3500);
+    }
+    if (touchendX >= touchstartX) {
+        if (slideIndex === 2) {
+            clearInterval(slideInterval);
+            currentSlide(slides.length);
+            slideInterval = setInterval(showSlides, 3500);
+        } else {
+            clearInterval(slideInterval);
+            currentSlide(slideIndex - 2);
+            slideInterval = setInterval(showSlides, 3500);
+        }
+    }
+}
+
+showSlides(slideIndex);
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        responsiveClass:true,
+        navText: [" ", " "],
+        margin:20,
+        responsive: {
+                0: {
+                    items:1,
+                    nav:true
+                },
+                920: {
+                    items:2,
+                    nav:true
+                },
+                1366: {
+                    items:3,
+                    nav:true
                 }
             }
-        ]
-    });
+        }
+    );
+
 });
+
 
 
